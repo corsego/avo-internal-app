355464a9cb0027716427dcba4401722291bf152d
Yaro Shm <yshmarov@gmail.com>
2024-11-23 12:40:22 +0100

    handle user invitations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   has_many :sessions, dependent: :destroy
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
+
+  before_validation :set_password, on: :create
+
+  def set_password
+    self.password = SecureRandom.hex
+  end
+
+  after_create_commit do
+    PasswordsMailer.reset(self).deliver_later
+  end
 end
